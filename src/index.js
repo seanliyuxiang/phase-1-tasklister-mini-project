5ea73ad1340512008f09194c4941d9ff62e61fe3
Yuxiang Li <seanliyuxiang@gmail.com>
2021-09-06 16:20:42 -0500

    Implemented basic deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // why selecting the outer form tag, why not selecting the inside submit input tag
+  let form = document.querySelector('form');
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    buildToDo(e.target['new-task-description'].value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let newTask = document.createElement('li');
+  let deleteBtn = document.createElement('button');
+  deleteBtn.addEventListener('click', handleDelete)
+  deleteBtn.innerText = ' x';
+  newTask.innerText = `${todo} `;
+  newTask.appendChild(deleteBtn);
+  document.querySelector('#tasks').appendChild(newTask);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
